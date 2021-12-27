933b3d0a785dd885666e52537c588c9bafdb2472
RazeSoldier <razesoldier@outlook.com>
2021-12-27 22:14:46 +0800

    fix(test): 修复因更新词汇表造成测试不通过的问题

--- src/test/java/razesoldier/gdlbot/translation/EVEProperNounsTranslatorTests.java
+++ src/test/java/razesoldier/gdlbot/translation/EVEProperNounsTranslatorTests.java
@@ -20,7 +20,7 @@ class EVEProperNounsTranslatorTests {
         assertEquals("重导 小希", translator.translate("HML Cerberus"));
         assertEquals("优先级: 重导 小希/曲剑 > 隐秘加成T3 > 指挥驱逐舰 (鹳鸟s/Biforsts)  > 麒麟/手术刀 > 轻拦 > NBI 守夜/克勒斯",
                 translator.translate("Doctrine: HML Cerberus/Scimitar > Ninja Links > Cmd Destroyer Links (Storks/Biforsts)  > Kirin/Scalpels > Dictors > NBI Vigil/Keres"));
-        assertEquals("鹳鸟/天梯 links > 曲剑s > 轻拦 > 侦查舰 > 小希es (重导 FITS) > 斯威普s > NBI 守夜",
+        assertEquals("鹳鸟/天梯 links > 曲剑s > 轻拦 > 侦查舰 > 小希es (重导配置) > 斯威普s > NBI 守夜",
                 translator.translate("Stork/Bifrost links > Scimitars > Interdictors > Recons > Cerberuses (HML FITS) > Svipuls > NBI Vigils"));
     }
 
