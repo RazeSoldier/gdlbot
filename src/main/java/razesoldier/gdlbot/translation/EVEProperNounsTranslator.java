f60b63eddfe341a00cebae2a89dd403a288bc007
RazeSoldier <razesoldier@outlook.com>
2022-01-04 21:54:37 +0800

    feat: 改进EVEProperNounsTranslator
    只需在词汇表编写英文的单数小写形式，就可以应对首字母大写、首字母大写复数形式、复数形式、全部字母大写、全部字母大写复数形式的情况

--- src/main/java/razesoldier/gdlbot/translation/EVEProperNounsTranslator.java
+++ src/main/java/razesoldier/gdlbot/translation/EVEProperNounsTranslator.java
@@ -6,6 +6,9 @@
 
 package razesoldier.gdlbot.translation;
 
+import org.atteo.evo.inflector.English;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -22,7 +25,44 @@ class EVEProperNounsTranslator implements Translator {
     @Override
     public String translate(String source) {
         var ref = new AtomicReference<>(source);
-        glossary.forEach((en, zh) -> ref.getAndUpdate(s -> s.replace(en, zh)));
+        glossary.forEach((en, zh) -> {
+            // en为小写。需要计算出en的首字母大写、首字母大写复数形式、复数形式、全部字母大写、全部字母大写复数形式
+            String[] targets = {getPlural(capitalizeFirstLetter(en)), getPlural(en), capitalizeAllLetter(getPlural(en)),
+                    capitalizeFirstLetter(en), capitalizeAllLetter(en), en};
+            for (String target : targets) {
+                var before = ref.get();
+                var after = before.replace(target, zh);
+                // 如果替换成功则直接跳过剩下的target
+                if (!before.equals(after)) {
+                    ref.set(after);
+                    break;
+                }
+            }
+        });
         return ref.get();
+    }
+
+    /**
+     * 首字母大写
+     */
+    @NotNull
+    private static String capitalizeFirstLetter(@NotNull String source) {
+        var firstLetter = source.substring(0, 1);
+        return firstLetter.toUpperCase() + source.substring(1);
+    }
+
+    /**
+     * 获得source的复数形式
+     */
+    private static String getPlural(@NotNull String source) {
+        return English.plural(source);
+    }
+
+    /**
+     * 字母全部大写
+     */
+    @NotNull
+    private static String capitalizeAllLetter(@NotNull String source) {
+        return source.toUpperCase();
     }
 }
--- src/test/java/razesoldier/gdlbot/translation/EVEProperNounsTranslatorTests.java
+++ src/test/java/razesoldier/gdlbot/translation/EVEProperNounsTranslatorTests.java
@@ -10,6 +10,9 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class EVEProperNounsTranslatorTests {
@@ -18,10 +21,64 @@ class EVEProperNounsTranslatorTests {
         var translator = getInstance();
         assertEquals("重导", translator.translate("HML"));
         assertEquals("重导 小希", translator.translate("HML Cerberus"));
-        assertEquals("优先级: 重导 小希/曲剑 > 隐秘加成T3 > 指挥驱逐舰 (鹳鸟s/Biforsts)  > 麒麟/手术刀 > 轻拦 > NBI 守夜/克勒斯",
+        assertEquals("优先级: 重导 小希/曲剑 > 隐秘行动T3 加成 > 指挥驱逐 加成 (鹳鸟/Biforsts)  > 麒麟/手术刀 > 轻拦 > NBI 守夜/克勒斯",
                 translator.translate("Doctrine: HML Cerberus/Scimitar > Ninja Links > Cmd Destroyer Links (Storks/Biforsts)  > Kirin/Scalpels > Dictors > NBI Vigil/Keres"));
-        assertEquals("鹳鸟/天梯 links > 曲剑s > 轻拦 > 侦查舰 > 小希es (重导配置) > 斯威普s > NBI 守夜",
+        assertEquals("鹳鸟/天梯 加成 > 曲剑 > Inter轻拦 > 侦查舰 > 小希 (重导配置) > 斯威普 > NBI 守夜",
                 translator.translate("Stork/Bifrost links > Scimitars > Interdictors > Recons > Cerberuses (HML FITS) > Svipuls > NBI Vigils"));
+        assertEquals("Give me 轻拦 and 狞獾", translator.translate("Give me dictors and caracals"));
+        assertEquals("轻拦", translator.translate("dictor"));
+        assertEquals("聚焦涅槃隐轰", translator.translate("FVB Bombers"));
+    }
+
+    @Test
+    void testCapitalizeFirstLetter() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = getCapitalizeFirstLetterMethod();
+        assertEquals("Dictor", method.invoke(null, "dictor"));
+    }
+
+    @Test
+    void testCapitalizeFirstLetterAndPlural() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method capitalizeFirstLetterMethod = getCapitalizeFirstLetterMethod();
+        var s = capitalizeFirstLetterMethod.invoke(null, "dictor");
+        assertEquals("Dictors", getGetPluralMethod().invoke(null, s));
+    }
+
+    @Test
+    void testGetPlural() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        assertEquals("dictors", getGetPluralMethod().invoke(null, "dictor"));
+    }
+
+    @Test
+    void testCapitalizeAllLetter() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        assertEquals("DICTOR", getCapitalizeAllLetterMethod().invoke(null, "dictor"));
+    }
+
+    @Test
+    void testCapitalizeAllLetterAndPlural() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method pluralMethod = getGetPluralMethod();
+        var s = pluralMethod.invoke(null, "dictor");
+        assertEquals("DICTORS", getCapitalizeAllLetterMethod().invoke(null, s));
+    }
+
+    @NotNull
+    private Method getCapitalizeFirstLetterMethod() throws NoSuchMethodException {
+        Method method = EVEProperNounsTranslator.class.getDeclaredMethod("capitalizeFirstLetter", String.class);
+        method.setAccessible(true);
+        return method;
+    }
+
+    @NotNull
+    private Method getGetPluralMethod() throws NoSuchMethodException {
+        Method method = EVEProperNounsTranslator.class.getDeclaredMethod("getPlural", String.class);
+        method.setAccessible(true);
+        return method;
+    }
+
+    @NotNull
+    private Method getCapitalizeAllLetterMethod() throws NoSuchMethodException {
+        Method method = EVEProperNounsTranslator.class.getDeclaredMethod("capitalizeAllLetter", String.class);
+        method.setAccessible(true);
+        return method;
     }
 
     @NotNull
